42169d00fa8ebec7917e8538c4bff4f64ea661f6
PreranaUppalapati <51155654+PreranaUppalapati@users.noreply.github.com>
2023-06-12 17:25:45 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+
+
+inquirer
+  .prompt([ {
+    message: "Insert URL",
+    name: "URL",
+},
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr.png'));  
+  })
